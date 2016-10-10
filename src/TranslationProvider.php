6cb956f26e8f18326f9d9deb6d175d6a9f17dbb6
Dominik Zogg <dominik.zogg@gmail.com>
2016-10-10 20:34:51 +0200

    add declare(strict_types=1);

--- src/TranslationProvider.php
+++ src/TranslationProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Chubbyphp\Translation;
 
 use Pimple\Container;
--- src/TranslationTwigExtension.php
+++ src/TranslationTwigExtension.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Chubbyphp\Translation;
 
 final class TranslationTwigExtension extends \Twig_Extension
